d8fc636c7bd4a39c12b232fb924b4afa7516d53f
Pedro Paes <plpbs@poli.br>
2022-03-04 03:13:58 -0300

    Initial commit

---
+++ lib/spintria.ex
@@ -0,0 +1,18 @@
+defmodule Spintria do
+  @moduledoc """
+  Documentation for `Spintria`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Spintria.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/spintria_test.exs
@@ -0,0 +1,8 @@
+defmodule SpintriaTest do
+  use ExUnit.Case
+  doctest Spintria
+
+  test "greets the world" do
+    assert Spintria.hello() == :world
+  end
+end
