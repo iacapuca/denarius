09d1a1be7f09737f3b21bd08def9868e2b36c080
Pedro Paes <plpbs@poli.br>
2022-03-07 04:02:13 -0300

    Created Controllers

---
+++ lib/spintria/controllers/convert_controller.ex
@@ -0,0 +1,73 @@
+defmodule Spintria.Controller.ConvertController do
+  import Plug.Conn
+
+  alias Spintria.Schema.Transaction
+  alias Spintria.Utils.Clock
+
+  @exchange_api Application.compile_env!(:spintria, :exchange_api)
+
+  @doc """
+  Convert an amount of a certain currency to other currency
+  Returns a map which will be parsed to JSON by Jason
+  %{
+      "user_id" => 1,
+      "from" => "BRL",
+      "to" => "EUR",
+      "amount" => 100,
+      "rate" => rate,
+      "converted_amount" => 18.03729680083289,
+      "datetime_utc" => "2022-03-06T07:00:55.111485Z"
+  }
+  """
+  def store(conn) do
+    attrs = conn.assigns[:attrs]
+
+    {:ok, response} = @exchange_api.get("/latest", [])
+    rates = response.body["rates"]
+
+    converted_amount = convert_currency(rates, attrs.to, attrs.from, attrs.amount)
+
+    rate = converted_amount / attrs.amount
+
+    data = %{
+      "user_id" => attrs.user_id,
+      "from" => attrs.from,
+      "to" => attrs.to,
+      "amount" => attrs.amount,
+      "rate" => rate,
+      "converted_amount" => converted_amount,
+      "datetime_utc" => Clock.utc_now()
+    }
+
+    transaction = %Transaction{
+      :user_id => attrs.user_id,
+      :from => attrs.from,
+      :to => attrs.to,
+      :amount => attrs.amount,
+      :rate => rate
+    }
+
+    Spintria.Repo.insert!(transaction)
+
+    send_resp(conn, 200, Jason.encode!(data))
+  end
+
+  @doc """
+  Convert an amount of a certain currency to EUR
+  Returns the converted amount
+  """
+  defp convert_currency(rates, to, from, amount) when to == "EUR" do
+    converted_amount = amount / rates[from]
+    converted_amount
+  end
+
+  @doc """
+  Convert an amount of a certain currency to other currency
+  Returns the converted amount
+  """
+  defp convert_currency(rates, to, from, amount) do
+    eur_amount = amount / rates[from]
+    converted_amount = eur_amount * rates[to]
+    converted_amount
+  end
+end
---
+++ lib/spintria/controllers/transaction_controller.ex
@@ -0,0 +1,28 @@
+defmodule Spintria.Controller.TransactionController do
+  import Plug.Conn
+
+  alias Spintria.Schema.Transaction
+  alias Spintria.Repo
+  import Ecto.Query
+
+  @derive Jason.Encoder
+  defimpl Jason.Encoder, for: Transaction do
+    def encode(value, opts) do
+      Jason.Encode.map(
+        Map.take(value, [:transaction_id, :user_id, :amount, :from, :to, :inserted_at, :rate]),
+        opts
+      )
+    end
+  end
+
+  def index(conn) do
+    transactions = Transaction |> Repo.all()
+    send_resp(conn, 200, Jason.encode!(%{:transactions => transactions}))
+  end
+
+  def show(conn, user_id) do
+    q = from(t in Transaction, where: t.user_id == ^user_id, select: t)
+    transactions = Repo.all(q)
+    send_resp(conn, 200, Jason.encode!(%{:transactions => transactions}))
+  end
+end
