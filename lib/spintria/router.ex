344d9f4d1e3fdb45ab9fee2db7af7968cbabbb0f
Pedro Paes <plpbs@poli.br>
2022-03-07 04:01:56 -0300

    Created basic routes

---
+++ lib/spintria/router.ex
@@ -0,0 +1,45 @@
+defmodule Spintria.Router do
+  use Plug.Router
+  use Plug.ErrorHandler
+
+  alias Spintria.Plug.ValidatePlug
+  alias Spintria.Controller.ConvertController
+  alias Spintria.Controller.TransactionController
+
+  plug(Plug.Logger, log: :debug)
+  plug(ValidatePlug, paths: ["/convert"])
+  plug(:match)
+
+  plug(Plug.Parsers,
+    parsers: [:json],
+    pass: ["application/json", "text/json"],
+    json_decoder: Jason
+  )
+
+  plug(:dispatch)
+
+  get "/convert" do
+    ConvertController.store(conn)
+  end
+
+  get("/transactions") do
+    TransactionController.index(conn)
+  end
+
+  get("/transactions/:user_id") do
+    user_id = user_id |> String.to_integer()
+    TransactionController.show(conn, user_id)
+  end
+
+  match _ do
+    send_resp(conn, 404, "Not found")
+  end
+
+  defp handle_errors(conn, %{kind: _kind, reason: _reason, stack: _stack}) do
+    res = %{
+      "message" => conn.assigns[:error]
+    }
+
+    send_resp(conn, conn.status, Jason.encode!(res))
+  end
+end
