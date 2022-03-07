7b9099a527663d4f6e89fbfc5aa8a7e6ec748666
Pedro Paes <plpbs@poli.br>
2022-03-07 04:15:25 -0300

    Removed unusued code

--- lib/spintria.ex
+++ lib/spintria.ex
@@ -1,18 +1,2 @@
 defmodule Spintria do
-  @moduledoc """
-  Documentation for `Spintria`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Spintria.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
