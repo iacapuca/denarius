2a9e62278d105b0984bbb64f261cdda97f42b879
Pedro Paes <plpbs@poli.br>
2022-03-07 16:37:57 -0300

    Added Env setting

--- config/test.exs
+++ config/test.exs
@@ -1,9 +1,9 @@
 import Config
 
 config :denarius, Denarius.Repo,
-  database: "denarius_test",
-  username: "denarius",
-  password: "denarius",
+  database: System.get_env("POSTGRES_DATABASE") || "denarius_test",
+  username: System.get_env("POSTGRES_USER") || "denarius",
+  password: System.get_env("POSTGRES_PASSWORD") || "denarius",
   hostname: System.get_env("POSTGRES_HOST") || "localhost",
   show_sensitive_data_on_connection_error: true,
   pool: Ecto.Adapters.SQL.Sandbox
